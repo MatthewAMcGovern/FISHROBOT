96a6555799eaf813dc73e16bff38c5dbb8d9e5b3
DavidDePauw <daviddep@buffalo.edu>
2019-04-05 20:02:35 -0400

    help

--- Main_Arduino_Sketch/Main_Arduino_Sketch.ino
+++ Main_Arduino_Sketch/Main_Arduino_Sketch.ino
@@ -1,9 +1,85 @@
+<<<<<<< Updated upstream
 void setup() {
   // put your setup code here, to run once:
 
+=======
+#include <PID_v1.h>
+
+
+#include "struct.h"
+
+//DAVIDS STUFF
+//global
+byte potLpin = A2;
+byte potRpin;
+
+byte tailRpin;
+byte tailLpin = 3;
+
+//inst
+double potL;
+double potR;
+double potRprev;
+double potLprev;
+double potLfilt;
+double potRfilt;
+double potLfiltprev;
+double potRfiltprev;
+double prevT;
+double omega_c = 1;
+
+
+double sigR;
+double sigL;
+
+//targets for the tails
+double targR;
+double targL;
+
+//pid constants
+double kpR;
+double kiR;
+double kdR;
+
+double kpL;
+double kiL;
+double kdL;
+
+//error on tails
+double eR;
+double eL;
+double eRprev;
+double eLprev;
+
+int T;
+//END OF DAVIDS STUFF
+
+
+void setup() {
+  
+  Serial.begin(9600);
+  ballaskSetup();
+  T = micros();
+  
+  pinMode(potLpin, INPUT);
+  pinMode(tailLpin, OUTPUT);
+>>>>>>> Stashed changes
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+<<<<<<< Updated upstream
 
+=======
+  //heyLetsTestTheMotor();
+
+  Update(false);
+  //Serial.println(potL);
+  filter();
+  Serial.println(potLfilt);
+  int LSpeed = (int)((double)potL*((double)255/(double)1023));
+  writeToTails(LSpeed,0);
+  
+  
+>>>>>>> Stashed changes
 }
--- Main_Arduino_Sketch/Tail.ino
+++ Main_Arduino_Sketch/Tail.ino
@@ -1,1 +1,36 @@
 
+
+void Update(bool pid, set){
+  potRprev = potR;
+  potLprev = potL;
+  potL = analogRead(potLpin);
+  potR = analogRead(potRpin);
+  filter();
+
+  
+  
+}
+
+void setupTailSys(){
+  readPots();
+  filter();
+  
+}
+
+void filter(){
+  prevT = T;
+  T = (micros()-prevT)*1e-6;
+  potLfiltprev = potLfilt;
+  potLfilt = ((1.0-0.5*omega_c*T)/(1.0+0.5*omega_c*T))*potLfiltprev+(0.5*omega_c*T/(1.0+0.5*omega_c*T))*(potL+potLprev);
+  potRfiltprev = potRfilt;
+  potRfilt = ((1.0-0.5*omega_c*T)/(1.0+0.5*omega_c*T))*potRfiltprev+(0.5*omega_c*T/(1.0+0.5*omega_c*T))*(potR+potRprev);
+}
+
+void writeToTails(int LSpeed, int RSpeed){
+  analogWrite(tailLpin, LSpeed);
+  analogWrite(tailRpin, RSpeed);
+}
+
+
+
+
