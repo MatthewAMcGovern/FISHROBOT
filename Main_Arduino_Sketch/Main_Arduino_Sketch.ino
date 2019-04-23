27ad958830e6a8f85932e679ba944b2f9f62c2bb
DavidDePauw <daviddep@buffalo.edu>
2019-04-23 18:33:08 -0400

    Unstupided some things

--- Main_Arduino_Sketch/Main_Arduino_Sketch.ino
+++ Main_Arduino_Sketch/Main_Arduino_Sketch.ino
@@ -1,35 +1,62 @@
-#include <PID_v1.h>
+//describes all necessary data in one read cycle used to store raw and filtered data
+struct TailDataFrame{
+  double potL;
+  double potR;
+  double Lvel;
+  double Rvel;
+  double ReadTime;
+  double T;
+  
+};
 
+//describes all necessary data for determining PID output for given time step
+struct PIDDataFrame{
+  double LE;
+  double RE;
+  double REvel;
+  double LEvel;
+  double REint;
+  double LEint;
+  
+};
 
-#include "struct.h"
+//store data on setpoint behavior
+struct SetFrame{
+  double Lset;
+  double Rset;
+  double LSetVel;
+  double RSetVel;
+};
+
 
 //DAVIDS STUFF
 //global
 byte potLpin = A2;
-byte potRpin;
+byte potRpin = A3;
 
 byte tailRpin;
 byte tailLpin = 3;
 
-//inst
-double potL;
-double potR;
-double potRprev;
-double potLprev;
+//Read Glob vars
+
+const int s = 5;
+
+TailDataFrame hist[s];
+
+
+//Low pass
 double potLfilt;
 double potRfilt;
 double potLfiltprev;
 double potRfiltprev;
-double prevTi;
 double omega_c = 1;
 
-
-double sigR;
-double sigL;
 
 //targets for the tails
 double targR;
 double targL;
+
+
 
 //pid constants
 double kpR;
@@ -40,28 +67,56 @@ double kpL;
 double kiL;
 double kdL;
 
-//error on tails
-double eR;
-double eL;
-double eRprev;
-double eLprev;
+bool REngaged = false;
+bool LEngaged = false;
+bool RStartVel = false; //false for negative
+bool LStartVel = true; //true for positive
 
-int T;
-int ti;
+//error on tails
+
+
+//potentiometer bounds
+int LUbound;
+int LLbound;
+int RUbound;
+int RLbound;
+double Rmid;
+double Lmid;
+
+
+
+//TIME Glob vars
+double T;
+double TIME;
+double pTIME;
+
+//Moving Avg
+
+TailDataFrame Filt[s];
+
+
+//TEMP VAR
+
+int LSpeed;
+
+
 //END OF DAVIDS STUFF
 
 
 void setup() {
   
-  Serial.begin(9600);
-  ballaskSetup();
+  Serial.begin(9600);  
+  setupTailSys();
+//  delay(3000);
+//  LSpeed = 250;
+//  writeToTails(LSpeed,0);
+//  delay(7000);
+//  LSpeed = 80;
+//  writeToTails(LSpeed,0);
+//  delay(7000);
+  
+  
 
-  
-  
-  
-
- 
-  
 }
 
 void loop() {
@@ -69,10 +124,16 @@ void loop() {
   //heyLetsTestTheMotor();
 
   UpdateTailReading();
-  //Serial.println(potL);
+//  Serial.println(potL);
+//  LSpeed = 105;
+  Serial.print(hist[0].potR);
+  Serial.print(", ");
+  Serial.print(millis()/1000.0);
+  Serial.print(", ");
+  
 //  Serial.println(potLfilt);
-//  int LSpeed = (int)((double)potL*((double)255/(double)1023));
-//  writeToTails(LSpeed,0);
+  int LSpeed = (int)((double)hist[0].potR*((double)255/(double)1023));
+  writeToTails(LSpeed,0);
   
   
 }
--- Main_Arduino_Sketch/Tail.ino
+++ Main_Arduino_Sketch/Tail.ino
@@ -1,41 +1,185 @@
 
-
 void UpdateTailReading(){
-  potRprev = potR;
-  potLprev = potL;
-  potL = analogRead(potLpin);
-  potR = analogRead(potRpin);
-  filter();
+  TailDataFrame loc;
+  pTIME = TIME;
+  TIME = micros()*1e-6;
+  T = TIME - pTIME;
+  loc.potL = analogRead(potLpin);
+  loc.potR = analogRead(potRpin);
+  
+  loc.T = T;
+  loc.ReadTime = TIME;
+  loc.Lvel = (loc.potL - hist[0].potL)/loc.T;
+  loc.Rvel = (loc.potR - hist[0].potR)/loc.T;
+  
+  HistBuff(loc);
+  //filter();
+  MovingAvgFilter();
+  
+}
 
+void HistBuff(TailDataFrame tdf){
+  for(int i = 0; i < s-1; i++){
+    hist[i+1] = hist[i];
+  }
+  hist[0] = tdf;
+}
+
+void FiltBuff(TailDataFrame tdf){
+  for(int i = 0; i < s-1; i++){
+    Filt[i+1] = Filt[i];
+  }
+  Filt[0] = tdf;
 }
 
 void setupTailSys(){
   pinMode(potLpin, INPUT);
   pinMode(tailLpin, OUTPUT);
-  ti = micros();
-  potL = analogRead(potLpin);
-  potR = analogRead(potRpin);
-  potLfiltprev = PotL;
-  potRfiltprev = PotR;
+  pinMode(potRpin, INPUT);
+  pinMode(tailRpin, OUTPUT);
+  TIME = micros();
+  TailDataFrame loc;
+  loc.potL = analogRead(potLpin);
+  loc.potR = analogRead(potRpin);
+  loc.T = 0;
+  loc.ReadTime = TIME;
+  for(int i = 0; i < s; i++){
+    hist[i] = loc;
+  }
   UpdateTailReading();
   
 }
 
-void filter(){
-  prevTi = ti;
-  ti = micros();
-  T = (ti-prevT)*1e-6;
-  potLfiltprev = potLfilt;
-  potLfilt = ((1.0-0.5*omega_c*T)/(1.0+0.5*omega_c*T))*potLfiltprev+(0.5*omega_c*T/(1.0+0.5*omega_c*T))*(potL+potLprev);
-  potRfiltprev = potRfilt;
-  potRfilt = ((1.0-0.5*omega_c*T)/(1.0+0.5*omega_c*T))*potRfiltprev+(0.5*omega_c*T/(1.0+0.5*omega_c*T))*(potR+potRprev);
+//needs conversion to use buffer
+//also needs setup support code
+void LowPassFilter(){
+//  potLfiltprev = potLfilt;
+//  potLfilt = ((1.0-0.5*omega_c*T)/(1.0+0.5*omega_c*T))*potLfiltprev+(0.5*omega_c*T/(1.0+0.5*omega_c*T))*(potL+potLprev);
+//  potRfiltprev = potRfilt;
+//  potRfilt = ((1.0-0.5*omega_c*T)/(1.0+0.5*omega_c*T))*potRfiltprev+(0.5*omega_c*T/(1.0+0.5*omega_c*T))*(potR+potRprev);
+}
+
+//assumes hist is up to date on current cycle
+void MovingAvgFilter(){
+  TailDataFrame loc;
+  double RAvg = 0;
+  double LAvg = 0;
+  double avgSum = 0;
+  
+  for(int i = 0; i < s; i++){
+    RAvg += hist[i].potR * 1.0/((double)i+1);
+    LAvg += hist[i].potL * 1.0/((double)i+1);
+    avgSum += 1.0/((double)i+1);
+  }
+  loc.potR = RAvg/avgSum;
+  loc.potL = LAvg/avgSum;
+  loc.T = hist[0].T;
+  loc.Lvel = (loc.potL - Filt[0].potL)/loc.T;
+  loc.Rvel = (loc.potR - Filt[0].potR)/loc.T;
+  loc.ReadTime = hist[0].ReadTime;
+  FiltBuff(loc);
+  
 }
 
 void writeToTails(int LSpeed, int RSpeed){
-  analogWrite(tailLpin, LSpeed);
-  analogWrite(tailRpin, RSpeed);
+  if(LSpeed >= 0){
+    if(LSpeed > 130){
+      LSpeed = 130;
+      
+    }
+    else if(LSpeed < 80){
+      LSpeed = 80;
+    }
+    Serial.println(LSpeed);
+    analogWrite(tailLpin, LSpeed);
+  }
+
+  if(RSpeed >= 0){
+    if(RSpeed > 130){
+      RSpeed = 130;
+    }
+    else if(RSpeed < 80){
+    RSpeed = 80;
+    }
+
+    
+    analogWrite(tailRpin, RSpeed);
+  }
 }
 
 
+///////////////////////////////
+//PID Loop Logic
 
+// setpoint and error calculated externally, only error is needed
+double pid(double e, double kp, double ki, double kd, TailDataFrame* data){
+  //double out = kp*e + kd*(e - eprev)/T
+}
+
+//done every cycle once PID is engaged and passed to pid function
+void updateErrorBuff(double Lset, double Rset, TailDataFrame* data){
+  PIDDataFrame loc;
+  
+
+}
+
+//done every cycle before error frame is calculated
+void updateSetpointBuff(){
+  SetFrame loc;
+  
+  
+}
+
+//used to correlate frequency and pwm output
+double BaseOutput(double freq){
+  // need regression for baseline guess on certain frequency
+  
+}
+
+void moveToStartL(TailDataFrame* data){
+  if(!LEngaged){
+    writeToTails(105,-1);
+    if(!(data[0].Lvel > 0 ^ LStartVel)){
+      if(LStartVel){
+        if(data[0].potL >= Lmid){
+          LEngaged = true;
+          writeToTails(0,-1);
+        }
+      }
+      else{
+        if(data[0].potL <= Lmid){
+          LEngaged = true;
+          writeToTails(0, -1);
+        }
+      }
+    }
+  }
+  else{
+    writeToTails(0,-1);
+  }
+}
+
+void moveToStartR(TailDataFrame* data){
+  if(!REngaged){
+    writeToTails(-1, 105);
+    if(!(data[0].Rvel > 0 ^ RStartVel)){
+      if(LStartVel){
+        if(data[0].potR >= Rmid){
+          REngaged = true;
+          writeToTails(-1, 0);
+        }
+      }
+      else{
+        if(data[0].potR <= Rmid){
+          REngaged = true;
+          writeToTails(-1, 0);
+        }
+      }
+    }
+    
+  }
+  else{
+    writeToTails(-1, 0);
+  }
+}
 
